caf3196f8fef9ebf5182dc6a960cdf8a164491ce
Eric Lagergren <ericscottlagergren@gmail.com>
2015-11-27 21:46:36 -0800

    uint32 -> int63

--- client.go
+++ client.go
@@ -2,8 +2,6 @@ package jsonclient
 
 import (
 	"bytes"
-	"encoding/json"
-	"math/rand"
 	"net/http"
 
 	"github.com/gorilla/rpc/v2/json2"
@@ -19,37 +17,9 @@ type Client struct {
 	addr string
 }
 
-// clientRequest represents a JSON-RPC request sent by a client.
-type clientRequest struct {
-	// JSON-RPC protocol.
-	Version string `json:"jsonrpc"`
-
-	// A String containing the name of the method to be invoked.
-	Method string `json:"method"`
-
-	// Object to pass as request parameter to the method.
-	Params interface{} `json:"params"`
-
-	// The request id. This can be of any type. It is used to match the
-	// response with the request that it is replying to.
-	ID uint32 `json:"id"`
-}
-
-// EncodeClientRequest encodes parameters for a JSON-RPC client request with
-// an upper bound on the ID of (^uint32(0) - 1) / 2.
-func EncodeClientRequest(method string, args interface{}) ([]byte, error) {
-	c := &clientRequest{
-		Version: "2.0",
-		Method:  method,
-		Params:  args,
-		ID:      rand.Uint32() % ((^uint32(0) - 1) / 2),
-	}
-	return json.Marshal(c)
-}
-
 // Call invokes the named function, waits for it to complete, and returns its error status.
 func (c Client) Call(serviceMethod string, args interface{}, reply interface{}) error {
-	buf, err := EncodeClientRequest(serviceMethod, args)
+	buf, err := json2.EncodeClientRequest(serviceMethod, args)
 	if err != nil {
 		return err
 	}
